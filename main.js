1d1a3dd8c01ca5bcb9f49c4d066ad50f4fdc8276
tatafortunato <107056107+tatafortunato@users.noreply.github.com>
2024-03-22 08:48:12 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -15,10 +15,22 @@ for(let i=0;i <botoes.length;i++){
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date ("2004-10-05T00:00:00")
-let tempoAtual = new Date ();
+const tempoObjetivo1 = new Date ("2004-10-05T00:00:00");
+const tempoObjetivo2 = new Date ("2004-12-05T00:00:00");
+const tempoObjetivo3 = new Date ("2004-12-30T00:00:00");
+const tempoObjetivo4 = new Date ("2004-02-01T00:00:00");
 
-contadores[0].textcontent =  calculaTempo(tempoObjetivo1);
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+
+contadores[0].textcontent =  calculaTempo(tempos [0]);
+
+function atualizaCronametro(){
+      for (let i=0; i<contadores.length;i++){
+    contadores[i].textcontent =  calculaTempo(tempos[i]);
+}
+} 
+
+atualizaCronametro();
 
 function calculaTempo(tempoObjetivo){
 let tempoAtual = new Date();
